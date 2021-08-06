c6824cbae2b2087b084b963da1605ba0c8cb3cf5
SilverHu <hchuanyin@163.com>
2021-08-06 15:46:59 +0800

    Update commons.js
    修复1.9.2+ mock script断言脚本不能执行的问题

--- server/utils/commons.js
+++ server/utils/commons.js
@@ -586,7 +586,8 @@ ${JSON.stringify(schema, null, 2)}`)
     // script 是断言
     if (script) {
       logs.push('执行脚本:' + script)
-      result = await sandboxFn(context, script);
+      // result = await sandboxFn(context, script);
+      result = yapi.commons.sandbox(context, script);
     }
     result.logs = logs;
     return yapi.commons.resReturn(result);
